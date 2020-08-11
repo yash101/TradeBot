a70201a42738f9d9fee50fc9221aaf04d13db66a
yash101 <lodhad@gmail.com>
2020-08-10 21:18:55 -0700

    pushing changes so far with the account management:

--- Broker/Account.py
+++ Broker/Account.py
@@ -6,14 +6,65 @@ class AccountType(Enum):
     MARGIN = 2
 
 class Account():
-    def __init__(self):
-        self.nonMarginBuyingPower = 0
-        self.totalBuyingPower = 0
-        self.accountValue = 0
+    def __init__(
+        self,
+        accountIdentifier = None,
+        nonMarginBuyingPower = 0,
+        totalBuyingPower = 0,
+        accountValue = 0,
+        dayTradingAllowed = False,
+        dayTradesAvailable = 0,
+        positions = [],
+        accountType = AccountType.CASH
+    ):
+        self.account_identifier = accountIdentifier
+
+        self.non_margin_buying_power = nonMarginBuyingPower
+        self.total_buying_power = totalBuyingPower
+        self.account_value = accountValue
         
-        self.dayTradingAllowed = False
-        self.dayTradesAvailable = 0
+        self.day_trading_allowed = dayTradingAllowed
+        self.day_trades_available = dayTradesAvailable
 
-        self.accountType = AccountType.CASH
+        self.account_type = accountType
 
-        self.positions = []
+        self._positions = []
+    
+    def fetchUpdates(self):
+        pass
+
+    def accountIdentifier(self, newId = None):
+        if newId is not None:
+            self.account_identifier = newId
+        
+        return self.account_identifier
+    
+    def nonMarginBuyingPower(self, bp = None):
+        if bp is not None:
+            self.non_margin_buying_power = bp
+        
+        return self.non_margin_buying_power
+    
+    def totalBuyingPower(self, bp = None):
+        if bp is not None:
+            self.total_buying_power = bp
+        
+        return self.total_buying_power
+    
+    def accountValue(self, val = None):
+        if val is not None:
+            self.account_value = val
+        
+        return self.account_value
+    
+    def dayTradingAllowed(self, allowed = None):
+        if allowed is not None:
+            self.day_trading_allowed = allowed
+        
+        return self.day_trading_allowed
+    
+    def dayTradesAvailable(self, avail = None):
+        if avail is not None:
+            self.day_trades_available = avail
+
+        return self.day_trades_available
