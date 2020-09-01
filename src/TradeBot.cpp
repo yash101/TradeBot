68a1a1537f172c1a37e9b87f97485544b890110f
yash101 <lodhad@gmail.com>
2020-08-31 19:15:25 -0700

    Uploading project so far

---
+++ src/TradeBot.cpp
@@ -0,0 +1,6 @@
+#include "ql/instruments/averagetype.hpp"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
---
+++ src/TradeBot.h
@@ -0,0 +1,14 @@
+#ifndef _TRADEBOT_H
+#define _TRADEBOT_H
+
+namespace tb
+{
+    class TradeBot
+    {
+    public:
+        TradeBot();
+        virtual ~TradeBot();
+    };
+}
+
+#endif
