44c3191d7338969889ecda7b4b44e5692357840a
yash101 <lodhad@gmail.com>
2020-09-06 19:09:36 -0700

    TradeBot main class

--- src/TradeBot.cpp
+++ src/TradeBot.cpp
@@ -1,8 +1,115 @@
-#include "ql/instruments/averagetype.hpp"
+#include "TradeBot.h"
+#include "Configuration.h"
+#include "database.h"
+
+#include <sstream>
+#include <iostream>
+
+
+tb::TradeBot::TradeBot(int argc, char** argv)
+{
+    process_args(argc, argv);
+    print_help();
+    startup_db();
+}
+
+
+tb::TradeBot::~TradeBot()
+{
+}
+
+
+void
+tb::TradeBot::process_args(
+    int argc,
+    char** argv
+)
+{
+    for (int i = 1; i < argc; i++)
+    {
+        std::istringstream strm(argv[i]);
+        std::string k;
+        std::getline(strm, k, '=');
+        cmdline_args[k] = strm.str();
+    }
+}
+
+
+void
+tb::TradeBot::startup_db()
+{
+    std::string host, port, options, dbname, login, password;
+
+#define setif(var, cmdline, cfg) var = (check_cmdline_arg(cmdline)) ? get_cmdline_arg(cmdline) : std::string(cfg)
+
+    setif(host, "postgres.host", DB_HOST);
+    setif(port, "postgres.port", std::to_string(DB_PORT));
+    setif(options, "postgres.options", DB_OPTIONS);
+    setif(dbname, "postgres.dbname", DB_NAME);
+    setif(login, "postgres.login", DB_LOGIN);
+    setif(password, "postgres.password", DB_PASSWORD);
+
+    tb::db::pool_instance().configure(
+        host,
+        port,
+        options,
+        "",
+        dbname,
+        options,
+        password
+    );
+
+#undef setif
+}
+
+
+void
+tb::TradeBot::print_help()
+{
+    if (!(check_cmdline_arg("--help") || check_cmdline_arg("-h") || check_cmdline_arg("help")))
+        return; // no help needed
+
+#define hlp(opt, msg) std::cout << "\t" << opt << "\t" << msg << std::endl
+#define sect(name) std::cout << std::endl << name << " Configuration:" << std::endl << std::endl
+
+    std::cout << "Usage: TradeBot [arg1]=[value] [arg2]=[value]" << std::endl;
+
+    sect("Database");
+    hlp("postgres.host", "hostname or domain name for the postgres database server");
+    hlp("postgres.port", "port at which to connect to the database server");
+    hlp("postgres.options", "options for the connection to the database server");
+    hlp("postgres.dbname", "name of the database");
+    hlp("postgres.login", "username to login with");
+    hlp("postgres.password", "password to login with");
+
+#undef sect
+#undef hlp
+}
+
+
+std::string
+tb::TradeBot::get_cmdline_arg(
+    std::string key
+)
+{
+    return cmdline_args[key];
+}
+
+
+bool
+tb::TradeBot::check_cmdline_arg(
+    std::string key
+)
+{
+    return cmdline_args.find(key) != cmdline_args.end();
+}
+
 
 /** \brief Entry point for TradeBot
 */
 int main(int argc, char** argv)
 {
+    tb::TradeBot trbt(argc, argv);
+
     return 0;
 }
--- src/TradeBot.h
+++ src/TradeBot.h
@@ -1,22 +1,60 @@
 #ifndef _TRADEBOT_H
 #define _TRADEBOT_H
 
+#include <unordered_map>
+
 namespace tb
 {
     /** \brief The main class for TradeBot where the magic lives. This class contains references to all important parts of TradeBot and it's functionality drives the software
     */
     class TradeBot
     {
+    private:
+
+        std::unordered_map<std::string, std::string> cmdline_args;
+
+        void
+        print_help();
+
+        void
+        process_args(
+            int argc,
+            char** argv
+        );
+
+        void
+        startup_db();
+
     public:
 
         /** \brief Constructor
         */
-        TradeBot();
+        TradeBot(int argc, char** argv);
 
 
         /** \brief Destructor
         */
         virtual ~TradeBot();
+
+
+        /** \brief returns a command line argument used to launch TradeBot
+        * \param key is the value for the command line argument we are searching for
+        * \return the argument value
+        */
+        std::string
+        get_cmdline_arg(
+            std::string key
+        );
+
+
+        /** \brief checks if a command line argument was given
+        * \param key to look up
+        * \return true if the command line arg was given
+        */
+        bool
+        check_cmdline_arg(
+            std::string key
+        );
     };
 }
 
