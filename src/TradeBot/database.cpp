040dad273587cffd00ffb28a7e9b1df2f2eaf728
yash101 <lodhad@gmail.com>
2020-09-11 00:09:26 -0700

    Refactored TdAuthSvr main() to a different file and developed it as its own compilation unit

--- src/TradeBot/database.cpp
+++ src/TradeBot/database.cpp
@@ -381,6 +381,8 @@ tb::db::initialize_database(
 	PGresult* result;
 	ExecStatusType status;
 
+	// names of files in /res/sql
+	// run the query for each of these
 	std::vector<const char*> filenames{
 		"sql/configuration.sql",
 		"sql/equity_time_point.sql",
@@ -390,17 +392,20 @@ tb::db::initialize_database(
 		"sql/tdameritrade_order.sql",
 		"sql/tdameritrade_root_account.sql",
 		"sql/tdameritrade_trading_account.sql",
-		"sql/user.sql",
-		nullptr
+		"sql/user.sql"
 	};
 
+	// run each query
 	for (auto it = filenames.begin(); it != filenames.end(); ++it)
 	{
+		// get the string with the statement from the initial filename that was embedded
 		const char* stmt = tb::get_res(*it);
 
+		// print if we are being verbose
 		if (tb::TradeBot::instance().check_cmdline_arg("verbose"))
 			printf("Executing statement:\n%s\n", stmt);
 
+		// get the result
 		result = PQexec(
 			g(),
 			stmt
--- src/tdapi/authenticationsvr.cpp
+++ src/tdapi/authenticationsvr.cpp
@@ -9,56 +9,6 @@
 using json = nlohmann::json;
 
 
-// // this code can be built as an executable
-// #ifdef BUILD_TD_AUTHSERVER_SEPARATE
-
-// int main(int argc, char** argv)
-// {
-//     tb::tdameritrade::OneTimeOauthAgent agent;
-
-//     if (argc < 5)
-//     {
-//         printf("Usage: %s {listen ip} {listen port} {consumer key} {redirect uri}\n", argv[0]);
-//         return -1;
-//     }
-
-//     std::string
-//         listen_ip = argv[1],
-//         consumer_key = argv[3],
-//         redirect_uri = argv[4];
-
-//     short listen_port = std::atoi(argv[2]);
-
-//     char buffer[1024];
-//     snprintf(buffer, 1024, "http://%s:%d/redirect_uri", listen_ip.c_str(), listen_port);
-
-//     std::cout << "Now listening at http://" << listen_ip << ":" << listen_port << "/" << std::endl
-//         << "Please point your browser to the address above to authenticate" << std::endl;
-    
-//     auto ret = agent.authenticateOAuth(
-//         listen_ip,
-//         listen_port,
-//         std::string(buffer),
-//         consumer_key + "@AMER.OAUTHAP"
-//     );
-
-//     if (!std::get<0>(ret))
-//     {
-//         std::cout << "An error occurred during authentication" << std::endl;
-//         return -1;
-//     }
-
-//     std::cout << "Authorization Code: " << std::get<1>(ret) << std::endl
-//         << "Refresh Token: " << std::get<2>(ret) << std::endl
-//         << "Access Token: " << std::get<3>(ret) << std::endl;
-
-//     tb::db::initialize_database(*tb::db::PostgresConnectionGuard().get_connection());
-
-//     return 0;
-// }
-
-// #endif
-
 tb::tdameritrade::OneTimeOauthAgent::OneTimeOauthAgent()
 {
     web_server.Get("/", [&](const httplib::Request& req, httplib::Response& res) {
