a974851761e692568b7a3b3a521c0484625e8eaa
yash101 <lodhad@gmail.com>
2020-09-24 01:26:00 -0700

    nuking some sql. checking in in case i realize i miss this poor code

--- res/sql/configuration.sql
+++ res/sql/configuration.sql
@@ -3,3 +3,19 @@ CREATE TABLE IF NOT EXISTS "configuration" (
     val             TEXT,
     last_updated    TIMESTAMP       NOT NULL    default current_timestamp
 );
+
+CREATE OR REPLACE FUNCTION get_conf_or_update(conf_name, preferred_value, default_value)
+    RETURNS TEXT
+    LANGUAGE plpgsql
+    AS
+$$
+DECLARE
+    final_value TEXT;
+BEGIN
+    SELECT * INTO database_initial FROM 'configuration' WHERE opt = conf_name;
+
+    SELECT CASE WHEN 
+
+    SELECT COALESCE(database_initial.val, preferred_value, default_value, '') INTO first_not_null;
+END;
+$$
