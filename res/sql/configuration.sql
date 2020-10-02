3b2f1536564188396a2d1247a308e67c0803aa7f
yash101 <lodhad@gmail.com>
2020-10-01 18:55:55 -0700

    about to nuke this project out to hell

--- res/sql/configuration.sql
+++ res/sql/configuration.sql
@@ -4,18 +4,27 @@ CREATE TABLE IF NOT EXISTS "configuration" (
     last_updated    TIMESTAMP       NOT NULL    default current_timestamp
 );
 
-CREATE OR REPLACE FUNCTION get_conf_or_update(conf_name, preferred_value, default_value)
-    RETURNS TEXT
+CREATE OR REPLACE FUNCTION get_conf_or_update(conf_name, preferred_value)
+    RETURNS RECORD
     LANGUAGE plpgsql
     AS
 $$
 DECLARE
-    final_value TEXT;
+    cfg_option TEXT;
+    cfg_value  TEXT;
+    cfg_update TEXT;
+
+    ret        RECORD;
 BEGIN
-    SELECT * INTO database_initial FROM 'configuration' WHERE opt = conf_name;
-
-    SELECT CASE WHEN 
-
-    SELECT COALESCE(database_initial.val, preferred_value, default_value, '') INTO first_not_null;
+    INSERT INTO "configuration" (opt, val, last_updated)
+        VALUES (conf_name, preferred_value, NOW())
+        WHERE opt = conf_name AND val <> preferred_value
+        ON CONFLICT (opt)
+            DO UPDATE SET val = preferred_value, last_updated = NOW();
+    
+    RETURN QUERY
+        SELECT cfg.opt, cfg.val, cfg.last_updated
+            FROM "configuration" cfg
+            WHERE cfg.opt = conf_name;
 END;
 $$
