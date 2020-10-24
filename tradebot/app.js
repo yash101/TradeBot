9d763ad754e37a1e0b38a3aa051745c63a069a7b
yash101 <lodhad@gmail.com>
2020-10-24 04:33:25 +0000

    adding new express project

--- tradebot/app.js
+++ tradebot/app.js
@@ -1,65 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-const authRouter = require('./routes/authenticators');
-const config = require('./config');
-
-const passport = require('passport');
-const mongoose = require('mongoose');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
+const logger = require('morgan');
 
-const dotenv = require('dotenv');
-dotenv.config();
+const pgPool = require('./components/database/postgres');
+const config = require('./components/configuration/configuration');
 
-var app = express();
+const indexRouter = require('./components/webapi/index');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(
-  config.mongo_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-.then(() => console.log('connected to mongodb database.'))
-.catch(err => console.error(err));
-
-app.use(expressSession({
-  secret: process.env.AUTH_SESSION_SECRET || 'test secret',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressSession({
+  secret: config.get('session.secret', process.env.DEFAULT_SESSION_SECRET || 'secret'),
+  resave: false,
+  saveUninitialized: false,
+}));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/authentication/', authRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json({
-    status: false,
-    failure: err.message
-  })
-});
 
 module.exports = app;
