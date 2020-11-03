008d12faa6899c9bddb0ced99770424126c9ef9d
yash101 <lodhad@gmail.com>
2020-11-02 21:15:37 -0700

    database table creation

--- tradebot/components/database/users.js
+++ tradebot/components/database/users.js
@@ -2,3 +2,31 @@ const db = require('./postgres');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 
+class User {
+  constructor() {
+    this.ready = async () => {
+      return db.query(`
+        --- userid, username, email, name, role created
+        CREATE TABLE "user" (
+          id        BIGINT      GENERATED ALWAYS AS IDENTITY,
+          username  TEXT        NOT NULL,
+          email     TEXT,
+          fname     TEXT,
+          lname     TEXT,
+          role      TEXT,
+          created   TIMESTAMPTZ NOT NULL  DEFAULT current_timestamp
+        );
+
+        CREATE TABLE "auth_keys" (
+          keyid     BIGINT      GENERATED ALWAYS AS IDENTITY,
+          parentid  BIGINT      NOT NULL,
+          clientid  TEXT,
+          secret    TEXT,
+          created   TIMESTAMPTZ NOT NULL  DEFAULT current_timestamp,
+          lastlogin TIMESTAMPTZ           DEFAULT NULL,
+          CONSTRAINT fk_parentid  FOREIGN KEY clientid REFERENCES user(id)
+        );
+      `);
+    };
+  }
+}
