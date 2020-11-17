227dc99149e4c50be3ef3810e2cbd0dac1702820
yash101 <lodhad@gmail.com>
2020-11-17 01:23:55 -0700

    CORS hell

--- tradebot/components/webapi/webapi.js
+++ tradebot/components/webapi/webapi.js
@@ -19,7 +19,9 @@ module.exports = (async () => {
   let app = express();
 
   app.use(logger('dev'));
-  app.use(cors());
+  app.use(cors({
+    credentials: true,
+  }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(expressSession({
